f81dc857f6296de55cdf67b885a07c0d06eeb2b6
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-10-13 09:21:31 -0300

    config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import {Test} from './Test';
+import {createContext, useState, useEffect} from 'react';
 import {Home} from './pages/Home';
+import {NewRoom} from './pages/NewRoom';
+import { getAuth, signInWithPopup, GoogleAuthProvider,onAuthStateChanged  } from "firebase/auth";
+import {AuthContextProvider} from './contexts/AuthContext'
+
+
+import { BrowserRouter, Route } from 'react-router-dom';
+
+
+
+  
 function App() {
+ 
+   
+  
+       
+    
+    
+    
+
+  
   return (
-      
-          <Home/> 
-      
+          <BrowserRouter> 
+          <AuthContextProvider>
+          <Route path= "/" exact component = {Home} />  
+          <Route path= "/rooms/news" component = {NewRoom} />  
+          </AuthContextProvider>
+          </BrowserRouter>
+           
     
   );
 }
